3e89665fc5704138523b2465c81e13e1f37ab53b
Miligalichyan <166944899+Miligalichyan@users.noreply.github.com>
2024-09-25 07:35:16 -0700

    Add files via upload

---
+++ views/footer.php
@@ -0,0 +1,51 @@
+
+<footer>
+<div class="site">
+<h4>Site name</h4>
+<ul>
+<li><a href="facebook.com"><i class="fa-brands fa-square-facebook"></i></a></li>
+<li><a href="istagram.com"><i class="fa-brands fa-square-instagram"></i></a></li>
+<li><a href="telegra.com"><i class="fa-brands fa-telegram"></i></a></li>
+<li><a href="vk.ru"><i class="fa-brands fa-vk"></i></a></li>
+</li>
+</ul>
+</ul>
+
+
+</div>
+<div class="menu">
+	<div class="menu1">
+	<ul>
+		<li><h4>Menu</h4>
+		<ul>
+		<li><a href="myphotos.php"><h5>Add image</h5></a></li>
+		<li><a href="login.php"><h5>Login</h5></a></li>
+		<li><a href="registration.php"><h5>Registration</h5></a></li>
+		</li>
+		</ul>
+	</ul>
+	</div>
+	<div class="menu2">
+	<ul>
+		<li><h4>Menu</h4>
+		<ul>
+		<li><a href="myphotos.php"><h5>My photos</h5></a></li>
+		<li><a href="wishlist.php"><h5>Wishlist</h5></a></li>
+		<li><a href="index.php"><h5>Page</h5></a></li>
+		</li>
+		</ul>
+	</ul>
+	</div>
+	<div class="menu3">
+	<ul>
+		<li><h4>Menu</h4>
+		<ul>
+		<li><a href="index.php"><h5>Page</h5></a></li>
+		<li><a href="index.php"><h5>Page</h5></a></li>
+		<li><a href="index.php"><h5>Page</h5></a></li>
+		</li>
+		</ul>
+	</ul>
+	</div>
+</div>
+</footer>
---
+++ views/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel='stylesheet' href='gallery.css'>
+<link rel='stylesheet' href='fontawesome-free-6.4.0-web/css/all.min.css'>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title> Gallery </title>
+<meta charset='utf-8'>
+</head>
+<body>
+<header>
+<div class="title">
+<h1><a href="index.php">Gallery</a></h1>
+</div>
+<div class="links">
+<h3><a href="myphotos.php">My photos</a></h3>
+<h3><a href="wishlist.php">Wishlist</a></h3>
+<h3><span><a href="login.php">Login</a></span></h3>
+</div>
+
+</header>
